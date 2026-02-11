003507567afc6838b4c6138eed8b778cfdf2692f
afk-panda2004 <lyk.mig@mail.ru>
2026-02-11 23:09:09 +0300

    add admin_panel

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,16 @@
 <?php
 
+use App\Http\Controllers\Admin\Main\AdminController;
 use App\Http\Controllers\Main\IndexController;
+
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('main')->group(function (){
     Route::get('/', IndexController::class);
+});
+
+Route::prefix('admin')->group(function (){
+    Route::get('/', AdminController::class);
 });
 Auth::routes();
 
